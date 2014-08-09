af112c8117f1c513218c8f87bf89f5866195ead3
martingeuer <martin.geuer@gmail.com>
2014-08-09 15:21:53 +0200

    Rename

--- gwt-src/org/rapla/client/Rapla.java
+++ gwt-src/org/rapla/client/Rapla.java
@@ -59,10 +59,10 @@ public class Rapla implements EntryPoint {
 
 		// Add the nameField and sendButton to the RootPanel
 		// Use RootPanel.get() to get the entire body element
-//		RootPanel.get("nameFieldContainer").add(nameField);
-//		RootPanel.get("passwordFieldContainer").add(passwordField);
-//		RootPanel.get("sendButtonContainer").add(sendButton);
-//		RootPanel.get("errorLabelContainer").add(errorLabel);
+		RootPanel.get("nameFieldContainer").add(nameField);
+		RootPanel.get("passwordFieldContainer").add(passwordField);
+		RootPanel.get("sendButtonContainer").add(sendButton);
+		RootPanel.get("errorLabelContainer").add(errorLabel);
 
 		// Focus the cursor on the name field when the app loads
 		nameField.setFocus(true);
