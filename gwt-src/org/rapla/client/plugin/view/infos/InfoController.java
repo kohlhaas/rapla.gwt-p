0f7c763430e1d3dea7736390db6154755206778b
Jenny <Jenny@Jenny-NEU.fritz.box>
2014-12-10 18:30:28 +0100

    Selection handler for tab bar

--- gwt-src/org/rapla/client/plugin/view/infos/InfoController.java
+++ gwt-src/org/rapla/client/plugin/view/infos/InfoController.java
@@ -2,9 +2,16 @@ package org.rapla.client.plugin.view.infos;
 
 import java.util.List;
 
+import org.rapla.client.event.RaplaEventBus;
+import org.rapla.client.event.ViewChangedEvent;
 import org.rapla.client.plugin.view.ContentDrawer;
 import org.rapla.client.plugin.view.ViewPlugin;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.ListBox;
@@ -26,8 +33,24 @@ public class InfoController implements ContentDrawer {
 	    bar.addTab("Veranstaltungsinfos");
 	    bar.addTab("Resourcen");
 	    bar.addTab("Termine");
+	    
+	    
+
+   
 		FlowPanel content = new FlowPanel();
+		
+		bar.addSelectionHandler(new SelectionHandler<Integer>() {
+	        public void onSelection(SelectionEvent<Integer> event) {
+	          // Let the user know what they just did.
+	          Window.alert("You clicked tab " + event.getSelectedItem());
+	          
+	          // add all three different views here 
+	        }});
+		 
 		content.add(bar);
+		
+		
+		
 
 		return content;
 	}
