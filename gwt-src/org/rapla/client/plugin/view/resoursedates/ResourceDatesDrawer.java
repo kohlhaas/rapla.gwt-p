46c6f49f7817f2e835599bc79a83b4a30df315c1
Philipp Schwarz <pschwarz92@googlemail.com>
2014-12-15 21:39:08 +0100

    Creating first Content in ResourceDatesDrawer

--- gwt-src/org/rapla/client/plugin/view/resoursedates/ResourceDatesDrawer.java
+++ gwt-src/org/rapla/client/plugin/view/resoursedates/ResourceDatesDrawer.java
@@ -2,7 +2,9 @@ package org.rapla.client.plugin.view.resoursedates;
 
 import org.rapla.client.plugin.view.ContentDrawer;
 
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -10,10 +12,53 @@ public class ResourceDatesDrawer implements ContentDrawer {
 
 	@Override
 	public Widget createContent() {
-		FlowPanel fp = new FlowPanel();
-		Label test = new Label("test");
-		fp.add(test);
-		return fp;
+	    Integer height = (int) (Window.getClientHeight() * 0.95 * 0.8);
+		
+		FlowPanel mainContent = new FlowPanel();
+		
+		FlowPanel dateList = new FlowPanel();
+		dateList.setHeight(height + "px");
+		dateList.setStyleName("dateList");
+		
+		
+		Label test = new Label("Test");
+		dateList.add(test);
+		
+		
+		FlowPanel buttonBar = new FlowPanel();
+		buttonBar.setHeight(height + "px");
+		buttonBar.setStyleName("datesButtonBar");
+		
+
+		//Image buttonNextGap = new Image("button_luecke.png");
+		Label buttonNextGap = new Label(">>");
+		buttonNextGap.setStyleName("buttonsResourceDates");		
+		
+		//Image buttonGarbageCan = new Image("button_eimer.png");
+		Label buttonGarbageCan = new Label("X");
+		buttonGarbageCan.setStyleName("buttonsResourceDates");		
+		
+		//Image buttonPlus = new Image("button_plus.png");
+		Label buttonPlus = new Label("+");
+		buttonPlus.setStyleName("buttonsResourceDates");
+		
+		buttonBar.add(buttonPlus);
+		buttonBar.add(buttonGarbageCan);
+		buttonBar.add(buttonNextGap);
+		
+		
+		FlowPanel dateInfos = new FlowPanel();
+		dateInfos.setHeight(height + "px");
+		dateInfos.setStyleName("dateInfos");
+		
+		dateInfos.add(test);
+		
+		
+		mainContent.add(dateList);
+		mainContent.add(buttonBar);
+		mainContent.add(dateInfos);
+		
+		return mainContent;
 	}
 
 	@Override
