5971121f326ec029930014d47985e7f4dff37de3
Yvonne Scheffler <yv.scheffler@googlemail.com>
2014-12-17 13:25:19 +0100

    Embed first view

--- gwt-src/org/rapla/client/edit/reservation/controller/EditController.java
+++ gwt-src/org/rapla/client/edit/reservation/controller/EditController.java
@@ -1,7 +1,7 @@
 package org.rapla.client.edit.reservation.controller;
 
 import org.rapla.client.edit.reservation.GWTReservationController;
-import org.rapla.client.edit.reservation.view.RaplaEditView;
+import org.rapla.client.edit.reservation.view.*;
 import org.rapla.entities.domain.Reservation;
 import org.rapla.facade.RaplaComponent;
 import org.rapla.framework.RaplaContext;
@@ -19,17 +19,19 @@ public class EditController extends RaplaComponent implements GWTReservationCont
 
   final Logger logger = Logger.getLogger("EditController");
 
-  @Inject
+  //@Inject
   RaplaEditView raplaEditView;
 
   public EditController(RaplaContext context) {
     super(context);
+    raplaEditView = new AppointmentsEditView();
   }
 
   @Override
   public void edit(Reservation event, boolean isNew) throws RaplaException {
     logger.info("building the view");
     raplaEditView.init();
+    raplaEditView.setTitle("Termine");
     logger.info("view successfully created");
 
   }
