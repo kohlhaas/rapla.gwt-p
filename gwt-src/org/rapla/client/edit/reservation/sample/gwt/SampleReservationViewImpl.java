d565a44a7c92457442127f8d876c3d0c35e6c08b
Felix Last <mail@felixlast.de>
2015-01-09 20:13:58 +0100

    Add appointment planning view and custom css

--- gwt-src/org/rapla/client/edit/reservation/sample/gwt/SampleReservationViewImpl.java
+++ gwt-src/org/rapla/client/edit/reservation/sample/gwt/SampleReservationViewImpl.java
@@ -4,14 +4,20 @@ import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.ui.*;
+
 import org.rapla.client.base.AbstractView;
 import org.rapla.client.edit.reservation.sample.ReservationEditSubView;
 import org.rapla.client.edit.reservation.sample.SampleReservationView;
 import org.rapla.client.edit.reservation.sample.SampleReservationView.Presenter;
 import org.rapla.entities.domain.Allocatable;
+import org.rapla.entities.domain.Appointment;
+import org.rapla.entities.domain.Repeating;
 import org.rapla.entities.domain.Reservation;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.Locale;
 
 public class SampleReservationViewImpl extends AbstractView<Presenter> implements SampleReservationView<IsWidget> {
@@ -120,6 +126,135 @@ public class SampleReservationViewImpl extends AbstractView<Presenter> implement
                 getPresenter().changeEventName(tb.getText());
             }
         });
+        
+        
+        // Terminplanung View 1234
+        // Layout Panels
+        FlowPanel view = new FlowPanel();
+        view.setStyleName("felix-view");
+        content.add(view);
+        DateTimeFormat df = DateTimeFormat.getFormat("dd.MM.yyyy");
+        DateTimeFormat hoursFormat = DateTimeFormat.getFormat("HH");
+        DateTimeFormat minutesFormat = DateTimeFormat.getFormat("mm");
+        
+        // Appointment List
+        ListBox appointmentList = new ListBox();
+        appointmentList.setStyleName("appointment-list");
+        for(Appointment a : event.getAppointments()) {
+        	String appointmentLabel = df.format(a.getStart()) + " "; // + " - " + df.format(a.getEnd());
+        	appointmentList.addItem(appointmentLabel);
+        }
+        appointmentList.setVisibleItemCount(7);
+        view.add(appointmentList);
+        
+        // Temporary variable to hold currently selected appointment. Will be replaced by changehandler on listbox 
+        Appointment selectedAppointment = event.getAppointments()[0];
+        
+        // Rechte Seite des Termin Panels
+        FlowPanel appointmentOptionsPanel = new FlowPanel();
+        appointmentOptionsPanel.setStyleName("appointment-options");
+        view.add(appointmentOptionsPanel);
+        
+        // Repeat Radio Buttons
+        RadioButton[] selectRepeat = new RadioButton[5];
+        selectRepeat[0] = new RadioButton("select-repeat", "Nicht wiederholen");
+        selectRepeat[1] = new RadioButton("select-repeat", "Täglich");
+        selectRepeat[2] = new RadioButton("select-repeat", "Wöchentlich");
+        selectRepeat[3] = new RadioButton("select-repeat", "Monatlich");
+        selectRepeat[4] = new RadioButton("select-repeat", "Jährlich");
+        selectRepeat[0].setValue(true);
+        FlowPanel selectRepeatPanel = new FlowPanel();
+        appointmentOptionsPanel.add(selectRepeatPanel);
+        for(RadioButton repeatButton : selectRepeat) {
+        	selectRepeatPanel.add(repeatButton);
+        }
+        // Check the box according to selected appointment
+        Repeating repeat = selectedAppointment.getRepeating();
+        RadioButton checked = selectRepeat[0]; 
+        if(repeat != null) { 
+        	switch (repeat.getType()) {
+				case DAILY:
+					checked = selectRepeat[1];
+        			break;
+				case MONTHLY:
+					checked = selectRepeat[2];
+					break;
+				case WEEKLY:
+					checked = selectRepeat[3];
+					break;
+				case YEARLY:
+					checked = selectRepeat[4];
+					break;
+        	}
+        	
+        }
+        checked.setValue(true);
+        
+        // Einzeltermine Button
+        Button convertToSingleEventsButton = new Button("In Einzeltermine umwandeln");
+        appointmentOptionsPanel.add(convertToSingleEventsButton);
+        
+        // Formular zur Zeit- & Datumswahl 
+        FlowPanel appointmentDatesForm = new FlowPanel();
+        appointmentDatesForm.setStyleName("appointment-date-form");
+        appointmentOptionsPanel.add(appointmentDatesForm);
+        TextBox startDateField, endDateField, startHourField, startMinuteField, endHourField, endMinuteField;
+        
+        FlowPanel startFields = new FlowPanel();
+        startFields.setStyleName("start-datetime");
+        appointmentDatesForm.add(startFields);
+        
+        startDateField = new TextBox();
+        startDateField.setStyleName("date-field");
+        startDateField.setText(df.format( selectedAppointment.getStart() ));
+        startFields.add(startDateField);
+        
+        startHourField = new TextBox();
+        startHourField.setStyleName("time-field");
+        startHourField.setMaxLength(2);
+        startHourField.setVisibleLength(2);
+        startHourField.setText(hoursFormat.format( selectedAppointment.getStart() ));
+        startFields.add(startHourField);
+        
+        Label startTimeColon = new Label(" : ");
+        startTimeColon.setStyleName("time-field");
+        startFields.add(startTimeColon);
+        
+        startMinuteField = new TextBox();
+        startMinuteField.setStyleName("time-field");
+        startMinuteField.setMaxLength(2);
+        startMinuteField.setVisibleLength(2);
+        startMinuteField.setText(minutesFormat.format( selectedAppointment.getStart() ));
+        startFields.add(startMinuteField);
+        
+        FlowPanel endFields = new FlowPanel();
+        startFields.setStyleName("end-datetime");
+        appointmentDatesForm.add(endFields);
+        
+        endDateField = new TextBox();
+        endDateField.setStyleName("date-field");
+        endDateField.setText(df.format( selectedAppointment.getEnd() ));
+        endFields.add(endDateField);
+        
+        endHourField = new TextBox();
+        endHourField.setStyleName("time-field");
+        endHourField.setMaxLength(2);
+        endHourField.setVisibleLength(2);
+        endHourField.setText(hoursFormat.format( selectedAppointment.getEnd() ));
+        endFields.add(endHourField);
+        
+        Label endTimeColon = new Label(" : ");
+        endTimeColon.setStyleName("time-field");
+        endFields.add(endTimeColon);
+        
+        endMinuteField = new TextBox();
+        endMinuteField.setStyleName("time-field");
+        endMinuteField.setMaxLength(2);
+        endMinuteField.setVisibleLength(2);
+        endMinuteField.setText(minutesFormat.format( selectedAppointment.getEnd() ));
+        endFields.add(endMinuteField);
+        
+        
     }
 
     public void mapFromReservation(Reservation event) {
