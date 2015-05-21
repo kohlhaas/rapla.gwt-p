02ef208b81d09881cdb972f46060f14b1bb5506d
Felix Last <mail@felixlast.de>
2015-05-21 19:27:50 +0200

    addition to my last commit

--- gwt-src/org/rapla/client/edit/reservation/sample/ReservationPresenter.java
+++ gwt-src/org/rapla/client/edit/reservation/sample/ReservationPresenter.java
@@ -69,18 +69,12 @@ public class ReservationPresenter implements ReservationController, Presenter {
     @Override
     public void onSaveButtonClicked() {
         logger.info("save clicked");
-        logger.info(getConflicts().length + " conflicts found.");
-        if (getConflicts().length > 0) {
-            view.showConflicts(getConflicts());
-        } else {
-            try {
-                facade.store(reservation);
-            } catch (RaplaException e1) {
-                logger.error(e1.getMessage(), e1);
-            }
-            view.hide();
-            //view.hide();
+        try {
+            facade.store(reservation);
+        } catch (RaplaException e1) {
+            logger.error(e1.getMessage(), e1);
         }
+        view.hide();
     }
 
     public String getCurrentReservationName(Locale locale) {
